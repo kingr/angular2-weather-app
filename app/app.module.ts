96e78c6a7c2fca53f6b7c506945ef56084632808
Kurt Farao <kurt@liquidthought.co.za>
2016-10-17 18:02:47 +0200

    tutorial progress

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,45 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import {HeroesComponent} from './heroes.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService} from "./hero.service";
+import {DashboardComponent} from './dashboard.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
+  ],
+  bootstrap: [ AppComponent ],
+  providers: [HeroService]
 })
 export class AppModule { }
---
+++ app/dashboard.component.ts
@@ -0,0 +1,25 @@
+import {Component} from '@angular/core';
+
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-dashboard',
+  templateUrl: 'dashboard.component.html'
+})
+
+export class DashboardComponent {
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(data => this.heroes = data.slice(1,5))
+  }
+
+  gotoDetail(hero: Hero): void {}
+
+}
